ce4f3d48ea893e6b53783688f391c3fe4b42c515
Liam Tiernan <liamptiernan@gmail.com>
2020-10-09 19:49:35 -0400

    V1 UI
    Completed Version 1 of user interface. Simple 3 button design, plays to a score of 5. Simple CSS styling.

--- scripts.js
+++ scripts.js
@@ -1,3 +1,11 @@
+let userScore = 0;
+let computerScore = 0;
+
+function setScore (newUser,newComputer) {
+    userScore = newUser;
+    computerScore = newComputer;
+};
+
 function computerPlay () {
     // generates random number
     let random = Math.floor(Math.random()*3)+1;
@@ -13,13 +21,13 @@ function computerPlay () {
         case 3:
             return "scissors";
             break;
-    }               
-}
+    };               
+};
 
-function playRound (playerSelection, computerSelection) {
+function playRound (playerSelection) {
     // converts player selection to lowercase
     let playerLower = playerSelection.toLowerCase();
-
+    let computerSelection = computerPlay()
     // conditional checks player selection against computer selection
     // returns win or lose
     if (computerSelection==playerLower) {
@@ -50,64 +58,96 @@ function playRound (playerSelection, computerSelection) {
             case "rock":
                 return "You win! Rock breaks scissors!";
                 break;
-        }
-    }
-}    
-
-// determines winner of game
-function winnerTest (userScore, computerScore) {
-    // winner statement
-    let winnerStatement = `Congrats! You win! Your final score was ${userScore}.`;
-
-    // loser statement
-    let loserStatement = `You lose! Your final score was ${userScore}.`;
-
-    // if score is >2, return winner statement with score. if not, return loser statement with score.
-    if (userScore>computerScore) {
-        return winnerStatement;
-    } else if (userScore < computerScore) {
-        return loserStatement;
-    } else {
-        return `It's a tie. Your score was ${userScore}.`
-    };
-};  
-
-// plays 5 rounds of the game
-function game () {
-    // iteration counter
-    let counter
-
-    // total score
-    let userScore = 0
-    let computerScore = 0
-
-    // loop plays game 5 times
-    for (counter=0; counter<5; counter++) {
-        // user answer and computer answer
-        let userInput = prompt("Choose what to play: Rock, Paper, or Scissors:");
-        let computerInput = computerPlay();
-
-        // plays 1 round
-        let round = playRound(userInput,computerInput);
-
-        // checks result. If not lose, plus 1 to score, console log win statement. Else, console log lose statement.
-        if (round.search("lose!")==-1 && round.search("tie.")==-1 ) {
-            userScore++;
-            console.log(round);
-            console.log(userScore)
-            console.log(computerScore)
-        } else if (round.search("win!")==-1 && round.search("tie.")==-1) {
-            computerScore++;
-            console.log(round);
-            console.log(userScore)
-            console.log(computerScore)
-        } else {
-            console.log(round);
-            console.log(userScore)
-            console.log(computerScore)
         };
     };
+};
 
-    console.log(winnerTest(userScore,computerScore));
+function endGame (userScore, computerScore) {
+    const messageContainer = document.querySelector('#messages');
+    const message = document.querySelector('#message');
+    const scores = document.querySelector('#scores');
+
+    let winnerStatement = `Congrats! You win! The final score was ${userScore} to ${computerScore}.`;
+    let loserStatement = `You lose! The final score was ${userScore} to ${computerScore}.`;
+    let outcome
+    if (userScore>computerScore) {
+        outcome = winnerStatement;
+    } else {
+        outcome = loserStatement;
+    };
+
+    let againMessage = document.createElement('h2');
+    againMessage.textContent='Choose rock, paper, or scissors to play again.'
+   
+    message.textContent = outcome;
+    messageContainer.appendChild(againMessage);
+    scores.textContent = '';
+
+    setScore(0,0);
+};
+
+function createDisplay (userValue, computerValue) {
+    const scores = document.querySelector('#scores');
+    let userLabel = document.createElement('h3');
+    let computerLabel = document.createElement('h3');
+    let uScore = document.createElement('div');
+    let compScore = document.createElement('div');
+
+    userLabel.classList.add('score-labels');
+    userLabel.textContent = 'User Score';
+
+    computerLabel.classList.add('score-labels');
+    computerLabel.textContent = 'Computer Score';
+
+    uScore.textContent = userValue;
+    compScore.textContent = computerValue;
+
+    uScore.id ='user-score';
+    compScore.id='computer-score';
+
+    userLabel.appendChild(uScore);
+    computerLabel.appendChild(compScore);
+
+    scores.appendChild(userLabel);
+    scores.appendChild(computerLabel);
+}
+
+function checkResult (round) {
+    const message = document.querySelector('#message');
+    const messageContainer = document.querySelector('#messages');
+    const userDisplay = document.querySelector('#user-score');
+    const computerDisplay = document.querySelector('#computer-score');
+
+    if (messageContainer.childNodes[3]!=null) {
+        messageContainer.removeChild(messageContainer.childNodes[3])
+    };
+    // checks result. If not lose, plus 1 to score, console log win statement. Else, console log lose statement.
+    if (round.search("lose!")==-1 && round.search("tie.")==-1 ) {
+        userScore++;
+    } else if (round.search("win!")==-1 && round.search("tie.")==-1) {
+        computerScore++;
+    };
+
+    if (userDisplay===null) {
+        createDisplay(userScore, computerScore);
+        message.textContent = round;
+    } else {
+        message.textContent = round;
+        userDisplay.textContent = userScore;
+        computerDisplay.textContent = computerScore;
+    }
     
-}     
+    if (userScore>=5 | computerScore>=5) {
+        endGame(userScore,computerScore);
+    }
+};
+
+let choices = document.querySelectorAll('.choice');
+
+for (i=0; i<choices.length; i++){
+    choices[i].addEventListener('click', function (e) {
+        userInput = e.target.id;
+        round = playRound(userInput);
+        checkResult(round);
+    });
+};
